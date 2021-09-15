d993205219ae4d613db479fbae736dcb1765b445
luimar96 <luisa_martensson@hotmail.com>
2021-09-15 09:11:38 +0200

    character

--- src/components/CharacterComponent/CharacterListComponent.vue
+++ src/components/CharacterComponent/CharacterListComponent.vue
@@ -4,7 +4,14 @@
         <div class="character-container">
             <div class="character-card">
                 <div class="character-img">
-                    <img src="" alt="">
+                    <img src="../../assets/luke.jpg" alt="luke">
+                </div>
+                <div class="movie-information">
+                    <h3>Luke Skywalker</h3>
+                    <h3>30</h3>
+                </div>
+                <div class="card-button">
+                    <button>Reade more</button>
                 </div>
             </div>
         </div>
@@ -15,8 +22,21 @@
 export default {
     name:"CharacterListComponent",
     data:()=> ({
+        character:[],
+        characterImages:{},
         
     }),
+    async mounted(){
+        const url ="https://swapi.dev/api/people";
+        try{
+            const response = fetch(url);
+            const data = await response.json(url);
+        }
+        catch(error)
+        {
+            console.log(error)
+        }
+    }
 }
 </script>
 <style scoped>
