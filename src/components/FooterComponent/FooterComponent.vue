bc79e2f00c4e7b13fa4749268d2232cf5ef5f597
Nahd92 <dhan.marcellus@hotmail.com>
2021-09-20 16:05:11 +0200

    Footeralignment

--- src/components/FooterComponent/FooterComponent.vue
+++ src/components/FooterComponent/FooterComponent.vue
@@ -11,8 +11,12 @@
     <div class="about">
       <h3>About:</h3>
       <p>
-        We are 2 students Dhan and Luisa who are in their final year of newton competence education .NET program. During one of our courses' development of web applications. We were given a task of creating a vue app that uses the star wars api to retrieve information to the user. Our goal was to create a user friendly page for our star wars nerds where they can easily find information on various characters or movies.
-
+        We are 2 students Dhan and Luisa who are in their final year of newton
+        competence education .NET program. During one of our courses'
+        development of web applications. We were given a task of creating a vue
+        app that uses the star wars api to retrieve information to the user. Our
+        goal was to create a user friendly page for our star wars nerds where
+        they can easily find information on various characters or movies.
       </p>
     </div>
   </footer>
@@ -27,9 +31,10 @@ export default {
 <style lang="scss" scoped>
 @import "./style/styles.scss";
 
-
 .footer {
-  
+  display: flex;
+  justify-content: center;
+  align-items: center;
   text-align: center;
   height: 20em;
   background-color: var(--dark-color);
@@ -39,12 +44,12 @@ export default {
 }
 .about {
   text-align: center;
-  margin-top: 1em;
   margin-bottom: 0px;
 }
 .footer ul {
   display: flex;
   justify-content: center;
+  align-items: center;
 }
 .footer ul li i {
   cursor: pointer;
@@ -57,34 +62,33 @@ export default {
 }
 @media screen and (min-width: 900px) {
   .footer {
- display: flex;
- flex: wrap;
- justify-content: space-around;
-  background-color: var(--dark-color);
+    display: flex;
+    flex: wrap;
+    justify-content: space-around;
+    background-color: var(--dark-color);
+  }
+  .socials {
+    flex: 70%;
+    padding: 5em;
+  }
+  .socials ul {
+    display: flex;
+    padding: 1em;
+  }
+  .socials ul li i {
+    font-size: 2em;
+    padding-left: 0.2em;
+  }
+  .streck {
+    background-color: grey;
+    width: 0.2em;
+    opacity: 50;
+    height: 18em;
+    margin-top: 2em;
+  }
+  .about {
+    padding: 5em;
+    text-align: left;
+  }
 }
-.socials{
-  flex:70%;
-  padding: 5em;
-}
-.socials ul{
-  display: flex;
-  padding: 1em;
-}
-.socials ul li i{
-  font-size:2em;
-  padding-left: 0.2em;
-}
-.streck{
-  background-color:grey ;
-  width:0.2em;
-  opacity: 50;
-  height: 18em;
-  margin-top:2em;
-}
-.about{
-  padding: 5em;
-  text-align: left;
-}
-}
-
 </style>
